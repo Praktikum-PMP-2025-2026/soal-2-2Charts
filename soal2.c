aad4aa85d954ccf7e61a9640c4f2ca7e4b0976fb
2Charts <arvinbadri4@gmail.com>
2026-04-15 08:22:02 +0700

    submit soal 2

--- soal2.c
+++ soal2.c
@@ -1,5 +1,40 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * NAMA: ARVIN FAUZAN BADRI
+ * NIM:  13224072
+ * SOAL 2
+ */
+
+int batu(int n) {
+    if(n == 1) { // base case
+        return 0;
+    } 
+    
+    if (n % 2 == 0) { // genap
+        n /= 2;
+    }
+    else { // ganjil
+        n = 3*n + 1;
+    }
+
+    return 1 + batu(n);
+}
 
 int main(void) {
+    int T, *n;
+    scanf("%d", &T);
+
+    n = malloc(sizeof(*n) * T);
+
+    for(int i = 0; i < T; ++i) {
+        scanf("%d", &n[i]);
+    }
+
+    for(int i = 0; i < T; ++i) {
+        printf("LANGKAH %d\n", batu(n[i]));
+    }
+
     return 0;
 }
